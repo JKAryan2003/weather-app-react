23cd6dfc8b14d14e217642e9a4d224f0e29c12b5
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-03-19 12:20:40 +0530

    display weather is done with dummy data

---
+++ src/components/DisplayWeather.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './DisplayWeather.css'
+
+const DisplayWeather = () => {
+  return (
+    <div>
+      <h1 className='fw-bold fs-1 p-5'>MY WEATHER APP</h1>
+
+      <div className='text-center'>
+        <input type="text" placeholder='search city'/>
+      </div>
+
+      <div className='p-5'>
+        <p className='fw-semibold fs-2'>New York</p>
+        <p className='fs-5'>Wednesday 19 March</p>
+      </div>
+
+      <div className='row'>
+        <div className='d-flex col-md-6 border p-5'>
+          <i class="bi bi-cloud-fill fs-1"></i>
+          <div className='mx-5'>
+            <p className='fs-2'>5°</p>
+            <p className='fs-5'>Overcast Clouds</p>
+          </div>
+        </div>
+
+        <div className='row col-md-6 border p-5 fs-5'>
+          
+          <div className="col-md-4">
+            <div>
+              <p>6°</p>
+              <p>High</p>
+              <p>3°</p>
+              <p>Low</p>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div>
+              <p>3.1mph</p>
+              <p>Wind</p>
+              <p>70%</p>
+              <p>Rain</p>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div>
+              <p>6:40</p>
+              <p>Sunrise</p>
+              <p>7:19</p>
+              <p>Sunset</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default DisplayWeather
